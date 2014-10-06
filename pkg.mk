cea1a0433cc49c090fb46269c5e42ef1985e5919
Cyber MacGeddon <cybermaggedon@gmail.com>
2014-10-06 09:24:06 +0100

    Minor typos and stuff in packaging.

--- pkg.mk
+++ pkg.mk
@@ -1,11 +1,19 @@
 
+VERSION=0.50
+
 deb:
-	rm -rf cyberprobe-0.50
+	rm -rf cyberprobe-${VERSION}
 	autoreconf -fi
 	./configure
 	make dist
-	cp cyberprobe-0.50.tar.gz cyberprobe_0.50.orig.tar.gz
-	tar xvfz cyberprobe-0.50.tar.gz
-	cp -r debian/ cyberprobe-0.50/
-	cd cyberprobe-0.50/; dpkg-buildpackage -us -uc
+	cp cyberprobe-${VERSION}.tar.gz cyberprobe_${VERSION}.orig.tar.gz
+	tar xvfz cyberprobe-${VERSION}.tar.gz
+	cp -r debian/ cyberprobe-${VERSION}/
+	cd cyberprobe-${VERSION}/; dpkg-buildpackage -us -uc
+
+rpm:
+	make dist
+	rpmbuild --define "_topdir `pwd`/RPM" -ba cyberprobe.spec
+	cp cyberprobe-${VERSION}.tar.gz RPM/SOURCES/
+	rpmbuild --define "_topdir `pwd`/RPM" -ba cyberprobe.spec
 
