c09dc20b2886ae13a22b9fe888315ed0da640586
Cyber MacGeddon <cybermaggedon@gmail.com>
2014-10-12 16:47:20 +0100

    Made deb/rpm targets consistent in what they do.

--- pkg.mk
+++ pkg.mk
@@ -5,14 +5,16 @@ deb:
 	rm -rf cyberprobe-${VERSION}
 	autoreconf -fi
 	./configure
-	make dist
+	make distcheck
 	cp cyberprobe-${VERSION}.tar.gz cyberprobe_${VERSION}.orig.tar.gz
 	tar xvfz cyberprobe-${VERSION}.tar.gz
 	cp -r debian/ cyberprobe-${VERSION}/
 	cd cyberprobe-${VERSION}/; dpkg-buildpackage -us -uc
 
 rpm:
-	make dist
+	autoreconf -fi
+	./configure
+	make distcheck
 	mkdir -p RPM/SOURCES
 	cp cyberprobe-${VERSION}.tar.gz RPM/SOURCES/
 	rpmbuild --define "_topdir `pwd`/RPM" -ba cyberprobe.spec
