8d058e88df172625c850db6dcc54278374768cf5
Cyber MacGeddon <cybermaggedon@gmail.com>
2016-06-19 21:42:51 +0100

    Mainly documentation in code.

--- config/gaffer.lua
+++ config/gaffer.lua
@@ -1,9 +1,7 @@
 --
 -- Cybermon configuration file, used to tailor the behaviour of cybermon.
---
--- This configuration file stores events in ElasticSearch.  The event
--- functions are all empty stubs.  Maybe a good starting point for building
--- your own config from scratch.
+-- This one integrates cybermon with Gaffer, so that network events are
+-- stored in Gaffer.
 --
 
 -- This file is a module, so you need to create a table, which will be
@@ -15,7 +13,6 @@ local observer = {}
 local mime = require("mime")
 local jsenc = require("json.encode")
 local http = require("util.http")
-local jsenc = require("json.encode")
 
 -- Gaffer config -----------------------------------------------------------
 
@@ -38,7 +35,7 @@ if geoip then
   print("Using GeoIP: " .. tostring(geodb))
 end
 
--- Common data type URI ----------------------------------------------------
+-- RDF URI basesI ----------------------------------------------------------
 
 local cybtype = "http://cyberprobe.sf.net/type/"
 local cybprop = "http://cyberprobe.sf.net/prop/"
@@ -99,9 +96,6 @@ end
 -- Add an edge, with object type of string.
 -- s=subject, p=predicate, o=object
 local add_edge_s = function(edges, s, p, o)
-  if p == cybprop .. "body" then
-    return
-  end
   add_edge_basic(edges, "n:u:" .. s, "r:u:" .. p, "n:s:" .. o, "@r")
   add_edge_basic(edges, "n:u:" .. s, "n:s:" .. o, "r:u:" .. p, "@n")
 end
@@ -125,127 +119,150 @@ local submit_edges = function(edges)
   local c = http.http_req(gaffer_base .. "/graph/doOperation/add/elements",
   	                  "PUT", jsenc.encode(edges),
 			  "application/json")
+  -- Put an error to standard output, if standard HTTP response 204 not recvd
   if not (c == 204) then
     print("Gaffer REST error: status " .. c)
   end
 end
 
+-- Initialise.  This reaches out to the Gaffer instance, and stores some
+-- data type information.
 local init = function()
 
+  -- Create a JSON object to submit
   edges = {}
 
+  -- RDF type information for observation type.
   add_edge_u(edges, cybtype .. "observation", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybtype .. "observation", rdfs .. "label",
              "Observation")
 
+  -- RDF type information for liid type.
   add_edge_u(edges, cybtype .. "liid", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybtype .. "liid", rdfs .. "label",
-             "LIID")
+             "Device")
 
+  -- RDF type definition of method property.
   add_edge_u(edges, cybprop .. "method", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "method", rdfs .. "label",
              "Method")
 
+  -- RDF type definition of action property.
   add_edge_u(edges, cybprop .. "action", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "action", rdfs .. "label",
              "Action")
 
+  -- RDF type definition of code property.
   add_edge_u(edges, cybprop .. "code", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "code", rdfs .. "label",
              "Response code")
 
+  -- RDF type definition of status property.
   add_edge_u(edges, cybprop .. "status", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "status", rdfs .. "label",
              "Response status")
 
+  -- RDF type definition of url property.
   add_edge_u(edges, cybprop .. "url", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "url", rdfs .. "label",
              "URL")
 
+  -- RDF type definition of time property.
   add_edge_u(edges, cybprop .. "time", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybprop .. "time", rdfs .. "label",
              "Time of observation")
 
-  -- Geo
+  -- Geo, country property.
   add_edge_u(edges, cybtype .. "country", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybtype .. "country", rdfs .. "label",
              "Country of origin")
 
-  -- Protocol context
+  -- Protocol context.
   add_edge_u(edges, cybtype .. "context", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybtype .. "context", rdfs .. "label",
              "Protocol context")
 
-  -- For DNS
-  
+  -- For DNS, DNS message type (query, answer).
   add_edge_u(edges, cybprop .. "dns_type", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "dns_type", rdfs .. "label",
              "DNS type")
 
+  -- DNS query
   add_edge_u(edges, cybprop .. "query", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "query", rdfs .. "label",
              "DNS query")
 
+  -- DNS answer (for a name)
   add_edge_u(edges, cybprop .. "answer_name", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "answer_name", rdfs .. "label",
              "Answer (name)")
 
+  -- DNS answer (address)
   add_edge_u(edges, cybprop .. "answer_address", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "answer_address", rdfs .. "label",
              "Answer (address)")
 
-  -- Addresses
-
+  -- Addresses, source
   add_edge_u(edges, cybprop .. "source", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "source", rdfs .. "label",
              "Source address")
 
+  -- Destination address
   add_edge_u(edges, cybprop .. "dest", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "dest", rdfs .. "label",
              "Destination address")
 
+  -- ipv4 type, for ipv4 address nodes
   add_edge_u(edges, cybtype .. "ipv4", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybtype .. "ipv4", rdfs .. "label",
              "IPv4 address")
 
+  -- tcp type, for ip:tcp nodes
   add_edge_u(edges, cybtype .. "tcp", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybtype .. "tcp", rdfs .. "label",
              "TCP port")
 
+  -- udp type for ip:udp nodes
   add_edge_u(edges, cybtype .. "udp", rdf .. "type",
              rdfs .. "Resource")
   add_edge_s(edges, cybtype .. "udp", rdfs .. "label",
              "UDP port")
 
+  -- port property, integer port number which is assocatiated with port object
   add_edge_u(edges, cybprop .. "port", rdf .. "type",
              rdfs .. "Property")
   add_edge_s(edges, cybprop .. "port", rdfs .. "label",
              "Port number")
 
+  -- Send to Gaffer
   submit_edges(edges)
 
 end
 
+-- Observation ID, starts with 0, and is incremented.
+-- FIXME: Is this right? Cyberprobe gets restarted, this will trash existing
+-- observations.
 local next_id = 0
 
+-- Get next ID, increment last ID and return.
 local get_next_id = function()
   local id = next_id
   next_id = next_id + 1
@@ -269,13 +286,18 @@ local function get_stack(context, addrs, is_src)
   end
 
   -- FIXME: This is all very IPv4-centric.
+
+  -- IPv4 address
   if cls == "ipv4" then
     local p = {}
     p["id"] = cybobj .. "ipv4/" .. addr
     p["type"] = cybtype .. "ipv4"
     p["description"] = addr
+    -- Store IPv4 address.  This allows tcp/udp addresses to lookup their IP
+    -- address.
     p["ipv4"] = addr
 
+    -- Lookup IPv4 address in GeoIP for country code.
     if geodb then
       lookup = geodb:query_by_addr(addr)
       if lookup and lookup.code and not(lookup.code == "--") then
@@ -283,9 +305,11 @@ local function get_stack(context, addrs, is_src)
       end
     end
 
+    -- Store IPv4 protocol information in address list.
     table.insert(addrs, p)
   end
 
+  -- TCP address
   if cls == "tcp" then
     local p = {}
 
@@ -303,6 +327,7 @@ local function get_stack(context, addrs, is_src)
 
   end
 
+  -- UDP address
   if cls == "udp" then
     local p = {}
 
@@ -326,17 +351,25 @@ end
 -- Initialise a basic observation
 local create_basic = function(edges, context, action)
 
+  -- Get ID
   local id = get_next_id()
+
+  -- Get resource URI
   local uri = cybobj .. "obs/" .. id
+
+  -- URI -> observation type
   add_edge_u(edges, uri, rdf .. "type", cybtype .. "observation")
 
+  -- LIID information
   local liid = cybobj .. "liid/" .. context:get_liid()
   add_edge_u(edges, uri, cybprop .. "liid", liid)
   add_edge_u(edges, liid, rdf .. "type", cybtype .. "liid")
-  add_edge_s(edges, liid, rdfs .. "label", "LIID " .. context:get_liid())
+  add_edge_s(edges, liid, rdfs .. "label", "Device " .. context:get_liid())
 
+  -- Cyberprobe action type
   add_edge_s(edges, uri, cybprop .. "action", action)
 
+  -- Store protocol stack address information: source
   addrs = {}
   get_stack(context, addrs, true)
   for key, value in pairs(addrs) do
@@ -354,6 +387,7 @@ local create_basic = function(edges, context, action)
     add_edge_u(edges, uri, cybprop .. "source", value.id)
   end
 
+  -- Store protocol stack address information: destination
   addrs = {}
   get_stack(context, addrs, false)
   for key, value in pairs(addrs) do
@@ -371,12 +405,11 @@ local create_basic = function(edges, context, action)
     add_edge_u(edges, uri, cybprop .. "dest", value.id)
   end
   
+  -- Time, in xsd:dateTime format
   local tm = context:get_event_time()
   local tmstr = os.date("!%Y-%m-%dT%H:%M:%S", math.floor(tm))
   local millis = 1000 * (tm - math.floor(tm))
-
   tmstr = tmstr .. "." .. string.format("%03dZ", math.floor(millis))
-
   add_edge_dt(edges, uri, cybprop .. "time", tmstr)
 
   return uri
@@ -386,29 +419,24 @@ end
 -- This function is called when a trigger events starts collection of an
 -- attacker. liid=the trigger ID, addr=trigger address
 observer.trigger_up = function(liid, addr)
---  print("trigger_up")
 end
 
 -- This function is called when an attacker goes off the air
 observer.trigger_down = function(liid)
---  print("trigger_down")
 end
 
 -- This function is called when a stream-orientated connection is made
 -- (e.g. TCP)
 observer.connection_up = function(context)
---  print("connection_up")
 end
 
 -- This function is called when a stream-orientated connection is closed
 observer.connection_down = function(context)
---  print("connection_down")
 end
 
 -- This function is called when a datagram is observed, but the protocol
 -- is not recognised.
 observer.unrecognised_datagram = function(context, data)
---  print("unrecognised_datagram")
   local edges = {}
   local id = create_basic(edges, context, "unrecognised_datagram")
   add_edge_s(edges, id, cybprop .. "data", b64(data))
@@ -419,7 +447,6 @@ end
 -- This function is called when stream data  is observed, but the protocol
 -- is not recognised.
 observer.unrecognised_stream = function(context, data)
---  print("unrecognised_stream")
   local edges = {}
   local id = create_basic(edges, context, "unrecognised_stream")
   add_edge_s(edges, id, cybprop .. "data", b64(data))
@@ -429,7 +456,6 @@ end
 
 -- This function is called when an ICMP message is observed.
 observer.icmp = function(context, data)
---  print("icmp")
   local edges = {}
   local id = create_basic(edges, context, "icmp")
   add_edge_s(edges, id, cybprop .. "data", b64(data))
@@ -439,16 +465,20 @@ end
 
 -- This function is called when an HTTP request is observed.
 observer.http_request = function(context, method, url, header, body)
---  print("http_request")
   local edges = {}
   local id = create_basic(edges, context, "http_request")
   add_edge_s(edges, id, cybprop .. "method", method)
-  add_edge_u(edges, id, cybprop .. "url", url)
+  if not (url == nil) and not (url == "")
+    add_edge_u(edges, id, cybprop .. "url", url)
+  end
   for key, value in pairs(header) do
     add_edge_s(edges, id, cybprop .. "header:" .. key, value)
   end
-  if (body and not body == "") then
-    add_edge_s(edges, id, cybprop .. "body", b64(body))
+--  if (body and not body == "") then
+--    add_edge_s(edges, id, cybprop .. "body", b64(body))
+--  end
+  if url == nil then
+    url = ""
   end
   add_edge_s(edges, id, rdfs .. "label", "HTTP " .. method .. " " .. url)
   submit_edges(edges)
@@ -456,25 +486,29 @@ end
 
 -- This function is called when an HTTP response is observed.
 observer.http_response = function(context, code, status, header, url, body)
---  print("http_response")
   local edges = {}
   local id = create_basic(edges, context, "http_response")
   add_edge_s(edges, id, cybprop .. "code", code)
   add_edge_s(edges, id, cybprop .. "status", status)
-  add_edge_u(edges, id, cybprop .. "url", url)
+  if not (url == nil) and not (url == "")
+    add_edge_u(edges, id, cybprop .. "url", url)
+  end
   for key, value in pairs(header) do
     add_edge_s(edges, id, cybprop .. "header:" .. key, value)
   end
-  body = b64(body)
-  if (body) then
-    add_edge_s(edges, id, cybprop .. "body", body)
-  end
+--  body = b64(body)
+--  if (body) then
+--    add_edge_s(edges, id, cybprop .. "body", body)
+--  end
   -- If there's an image payload, use the payload as the thumbnail
   if header["Content-Type"] then
     if string.sub(header["Content-Type"], 1, 6) == "image/" then
       add_edge_u(edges, id, "http://dbpedia.org/ontology/thumbnail",
                  "data:" .. header["Content-Type"] .. ";base64," .. body)
     end
+  end
+  if url == nil then
+    url = ""
   end
   add_edge_s(edges, id, rdfs .. "label",
              "HTTP " .. code .. " " .. status .. " " .. url)
@@ -484,7 +518,6 @@ end
 
 -- This function is called when a DNS message is observed.
 observer.dns_message = function(context, header, queries, answers, auth, add)
---  print("dns_message")
   local edges = {}
   local id = create_basic(edges, context, "dns_message")
 
@@ -523,7 +556,6 @@ end
 
 -- This function is called when an FTP command is observed.
 observer.ftp_command = function(context, command)
---  print("ftp_command")
   local edges = {}
   local id = create_basic(edges, context, "ftp_command")
   add_edge_s(edges, id, cybprop .. "command", command)
@@ -533,7 +565,6 @@ end
 
 -- This function is called when an FTP response is observed.
 observer.ftp_response = function(context, status, text)
---  print("ftp_response")
   local edges = {}
   local id = create_basic(edges, context, "ftp_response")
   add_edge_s(edges, id, cybprop .. "status", status)
@@ -551,7 +582,6 @@ end
 
 -- This function is called when an SMTP command is observed.
 observer.smtp_command = function(context, command)
---  print("smtp_command")
   local edges = {}
   local id = create_basic(edges, context, "smtp_command")
   add_edge_s(edges, id, cybprop .. "command", command)
@@ -561,7 +591,6 @@ end
 
 -- This function is called when an SMTP response is observed.
 observer.smtp_response = function(context, status, text)
---  print("smtp_response")
   local edges = {}
   local id = create_basic(edges, context, "smtp_response")
   add_edge_s(edges, id, cybprop .. "status", status)
@@ -579,7 +608,6 @@ end
 
 -- This function is called when an SMTP response is observed.
 observer.smtp_data = function(context, from, to, data)
---  print("smtp_data")
   local edges = {}
   local id = create_basic(edges, context, "smtp_data")
   add_edge_s(edges, id, cybprop .. "from", from)
@@ -591,10 +619,10 @@ observer.smtp_data = function(context, from, to, data)
     t = t .. v
     add_edge_s(edges, id, cybprop .. "to", v)
   end
-  local body = b64(data)
-  if (body) then
-    add_edge_s(edges, id, cybprop .. "body", body)
-  end
+--  local body = b64(data)
+--  if (body) then
+--    add_edge_s(edges, id, cybprop .. "body", body)
+--  end
   add_edge_s(edges, id, rdfs .. "label", "SMTP " .. from .. " " .. t)
   submit_edges(edges)
 end
