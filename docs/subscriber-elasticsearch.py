22cbe5236b75ddd421c2b8c7294d1b8cd8d3e812
Cyber MacGeddon <cybermaggedon@gmail.com>
2016-07-23 18:26:24 +0100

    Started hacking

--- docs/subscriber-elasticsearch.py
+++ docs/subscriber-elasticsearch.py
@@ -12,253 +12,55 @@ import sys
 
 ############################################################################
 
-cybobj = "http://cyberprobe.sf.net/obj/"
-cybprop = "http://cyberprobe.sf.net/prop/"
-cybtype = "http://cyberprobe.sf.net/type/"
-rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
-rdfs = "http://www.w3.org/2000/01/rdf-schema#"
-
-rdftype = rdf + "type"
-rdfslabel = rdfs + "label"
-
-############################################################################
-
-def obj_uri(tp, id):
-    return cybobj + tp + "/" + id
-
-def prop_uri(prop):
-    return cybprop + prop
-
-def type_uri(t):
-    return cybtype + t
-
-############################################################################
-
-def add_string(obs, s, p, o):
-    obs.append(("u:" + s, "u:" + p, "s:" + o))
-
-def add_dt(obs, s, p, o):
-    obs.append(("u:" + s, "u:" + p, "d:" + o))
-
-def add_uri(obs, s, p, o):
-    obs.append(("u:" + s, "u:" + p, "u:" + o))
-
-def add_resource_type(obs, type, label):
-    add_uri(obs, type_uri(type), rdftype, rdfs + "Resource")
-    add_string(obs, type_uri(type), rdfslabel, label)
-
-def add_property(obs, type, label):
-    add_uri(obs, type_uri(type), rdftype, rdfs + "Property")
-    add_string(obs, type_uri(type), rdfslabel, label)
-
-############################################################################
-
 def init():
-    obs = []
-
-    # Observation type
-    add_uri(obs, type_uri("observation"), rdftype, rdf + "Resource")
-    add_string(obs, type_uri("observation"), rdfslabel, "Observation")
-
-    # Device type
-    add_uri(obs, type_uri("device"), rdftype, rdf + "Resource")
-    add_string(obs, type_uri("device"), rdfslabel, "Device")
-
-    # Device property on Observation.
-    add_uri(obs, prop_uri("device"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("device"), rdfslabel, "Device")
-
-    # Method property on Observation.
-    add_uri(obs, prop_uri("method"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("method"), rdfslabel, "Method")
-
-    # Action property on Observation.
-    add_uri(obs, prop_uri("action"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("action"), rdfslabel, "Action")
-
-    # Code property on Observation.
-    add_uri(obs, prop_uri("code"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("code"), rdfslabel, "Code")
-
-    # Command property on Observation.
-    add_uri(obs, prop_uri("command"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("command"), rdfslabel, "Command")
-
-    # Status property on Observation.
-    add_uri(obs, prop_uri("status"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("status"), rdfslabel, "Status")
-
-    # URL property on Observation.
-    add_uri(obs, prop_uri("url"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("url"), rdfslabel, "URL")
-
-    # Time property on Observation.
-    add_uri(obs, prop_uri("time"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("time"), rdfslabel, "Time")
-
-    # Country property on Observation.
-    add_uri(obs, prop_uri("country"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("country"), rdfslabel, "Country of origin")
-
-    # Message type property on Observation
-    add_uri(obs, prop_uri("type"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("type"), rdfslabel, "Type")
-
-    # DNS Query on Observation
-    add_uri(obs, prop_uri("query"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("query"), rdfslabel, "DNS Query")
-
-    # DNS Answer (name) on Observation
-    add_uri(obs, prop_uri("answer_name"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("answer_name"), rdfslabel, "Answer (name)")
-
-    # DNS Query on Observation
-    add_uri(obs, prop_uri("answer_address"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("answer_address"), rdfslabel, "Answer (address)")
-
-    # Protocol context
-    add_uri(obs, prop_uri("context"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("context"), rdfslabel, "Context")
-
-    # From property on Observation
-    add_uri(obs, prop_uri("from"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("from"), rdfslabel, "From")
-
-    # To property on Observation
-    add_uri(obs, prop_uri("to"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("to"), rdfslabel, "To")
-
-    # Source property on Observation
-    add_uri(obs, prop_uri("source"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("source"), rdfslabel, "Source address")
-
-    # Destination property on Observation
-    add_uri(obs, prop_uri("destination"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("destination"), rdfslabel, "Destination address")
-
-    # IP type
-    add_uri(obs, type_uri("ip"), rdftype, rdf + "Resource")
-    add_string(obs, type_uri("ip"), rdfslabel, "IP address")
-
-    # TCP type
-    add_uri(obs, type_uri("tcp"), rdftype, rdf + "Resource")
-    add_string(obs, type_uri("tcp"), rdfslabel, "TCP port")
-
-    # UDP type
-    add_uri(obs, type_uri("udp"), rdftype, rdf + "Resource")
-    add_string(obs, type_uri("udp"), rdfslabel, "UDP port")
-
-    output(obs)
-
-    # Port property on TCP and UDP
-    add_uri(obs, prop_uri("port"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("port"), rdfslabel, "Port")
-
-    # IP property on IP, TCP and UDP
-    add_uri(obs, prop_uri("ip"), rdftype, rdf + "Property")
-    add_string(obs, prop_uri("ip"), rdfslabel, "IP")
+    pass
 
 def output(obs):
-    edges = {}
-    edges["elements"] = []
-
-    for v in obs:
-
-        elt = {}
-        elt["directed"] = True
-        elt["class"] = "gaffer.data.element.Edge"
-        elt["group"] = "BasicEdge"
-        elt["source"] = "n:" + v[0]
-        elt["destination"] = "n:" + v[2]
-        elt["properties"] = {
-            "name": {
-                "gaffer.function.simple.types.FreqMap": {
-                    "@r": 1,
-                    "r:" + v[1]: 1
-                    }
-                }
-            }
-
-        edges["elements"].append(elt)
-
-        elt = {}
-        elt["directed"] = True
-        elt["class"] = "gaffer.data.element.Edge"
-        elt["group"] = "BasicEdge"
-        elt["source"] = "n:" + v[0]
-        elt["destination"] = "r:" + v[1]
-        elt["properties"] = {
-            "name": {
-                "gaffer.function.simple.types.FreqMap": {
-                    "@n": 1,
-                    "n:" + v[2]: 1
-                    }
-                }
-            }
-
-        edges["elements"].append(elt)
-
-    print json.dumps(edges, indent=4)
-
-#        print "%-75s %-65s %s" % (v[0], v[1], v[2])
-#    print
+    print(json.dumps(obs))
 
 ############################################################################
 
 def handle(msg):
 
+    # FIXME: Make a UUID up-stream.  How are we supposed to correlate things??
     id = str(uuid.uuid1())
-    obs = []
 
-    uri = obj_uri("obs", id)
-    add_string(obs, uri, rdftype, type_uri("observation"))
-    
-    add_string(obs, uri, prop_uri("action"), msg["action"])
-    add_uri(obs, uri, prop_uri("device"), obj_uri("device", msg["device"]))
+    observation = {
+        "id": id,
+        "action": msg["action"],
+        "device": msg["device"],
+        "time": msg["time"]
+        }
 
-    add_uri(obs, obj_uri("device", msg["device"]), rdftype, type_uri("device"))
-    add_string(obs, obj_uri("device", msg["device"]), rdfslabel,
-               "Device " + msg["device"])
-
-    add_dt(obs, uri, prop_uri("time"), msg["time"])
     if msg.has_key("method"):
-        add_string(obs, uri, prop_uri("method"), msg["method"])
+        observation["method"] = msg["method"]
     if msg.has_key("url"):
-        add_uri(obs, uri, prop_uri("url"), msg["url"])
+        observation["url"] = msg["url"]
     if msg.has_key("command"):
-        add_string(obs, uri, prop_uri("command"), msg["command"])
+        observation["command"] = msg["command"]
     if msg.has_key("status"):
-        add_string(obs, uri, prop_uri("status"), str(msg["status"]))
+        observation["status"] = msg["status"]
     if msg.has_key("text"):
-        t = ""
-        s = ""
-        for v in msg["text"]:
-            t = t + v + s
-            s = " "
-        add_string(obs, uri, prop_uri("text"), t)
+        observation["text"] = msg["text"]
     if msg.has_key("payload"):
         pass
     if msg.has_key("body"):
         pass
     if msg.has_key("from"):
-        add_string(obs, uri, prop_uri("from"), msg["from"])
+        observation["from"] = msg["from"]
     if msg.has_key("to"):
-        for v in msg["to"]:
-            add_string(obs, uri, prop_uri("to"), v)
+        observation["to"] = msg["to"]
     if msg.has_key("header"):
-        for k in msg["header"]:
-            add_string(obs, uri, prop_uri("header:" + k), msg["header"][k])
+        observation["header"] = msg["header"]
     if msg.has_key("type"):
-        add_string(obs, uri, prop_uri("type"), msg["type"])
+        observation["type"] = msg["type"]
     if msg.has_key("queries"):
-        for v in msg["queries"]:
-            add_string(obs, uri, prop_uri("query"), v)
+        observation["queries"] = msg["queries"]
     if msg.has_key("answers"):
-        for v in msg["answers"]:
-            if v.has_key("name"):
-                add_string(obs, uri, prop_uri("answer_name"), v["name"])
-            if v.has_key("address"):
-                add_string(obs, uri, prop_uri("answer_address"), v["address"])
+        observation["answers"] = msg["answers"]
+
+    observation["src"] = {}
+    observation["dest"] = {}
 
     if msg.has_key("src"):
         ip = None
