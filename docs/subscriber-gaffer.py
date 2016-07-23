77bf26e146bc47d22cee5c9cf00be13b5a2bb676
Cyber MacGeddon <cybermaggedon@gmail.com>
2016-07-23 17:49:32 +0100

    More hacking of the Python ZeroMQ subscription scripts

---
+++ docs/subscriber-gaffer.py
@@ -0,0 +1,318 @@
+#!/usr/bin/env python
+
+binding = "tcp://localhost:5555"
+
+############################################################################
+
+import zmq
+import json
+import uuid
+import sys
+
+############################################################################
+
+cybobj = "http://cyberprobe.sf.net/obj/"
+cybprop = "http://cyberprobe.sf.net/prop/"
+cybtype = "http://cyberprobe.sf.net/type/"
+rdf = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+rdfs = "http://www.w3.org/2000/01/rdf-schema#"
+
+rdftype = rdf + "type"
+rdfslabel = rdfs + "label"
+
+############################################################################
+
+def obj_uri(tp, id):
+    return cybobj + tp + "/" + id
+
+def prop_uri(prop):
+    return cybprop + prop
+
+def type_uri(t):
+    return cybtype + t
+
+############################################################################
+
+def add_string(obs, s, p, o):
+    obs.append(("u:" + s, "u:" + p, "s:" + o))
+
+def add_dt(obs, s, p, o):
+    obs.append(("u:" + s, "u:" + p, "d:" + o))
+
+def add_uri(obs, s, p, o):
+    obs.append(("u:" + s, "u:" + p, "u:" + o))
+
+def add_resource_type(obs, type, label):
+    add_uri(obs, type_uri(type), rdftype, rdfs + "Resource")
+    add_string(obs, type_uri(type), rdfslabel, label)
+
+def add_property(obs, type, label):
+    add_uri(obs, type_uri(type), rdftype, rdfs + "Property")
+    add_string(obs, type_uri(type), rdfslabel, label)
+
+############################################################################
+
+def init():
+    obs = []
+
+    # Observation type
+    add_uri(obs, type_uri("observation"), rdftype, rdf + "Resource")
+    add_string(obs, type_uri("observation"), rdfslabel, "Observation")
+
+    # Device type
+    add_uri(obs, type_uri("device"), rdftype, rdf + "Resource")
+    add_string(obs, type_uri("device"), rdfslabel, "Device")
+
+    # Device property on Observation.
+    add_uri(obs, prop_uri("device"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("device"), rdfslabel, "Device")
+
+    # Method property on Observation.
+    add_uri(obs, prop_uri("method"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("method"), rdfslabel, "Method")
+
+    # Action property on Observation.
+    add_uri(obs, prop_uri("action"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("action"), rdfslabel, "Action")
+
+    # Code property on Observation.
+    add_uri(obs, prop_uri("code"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("code"), rdfslabel, "Code")
+
+    # Command property on Observation.
+    add_uri(obs, prop_uri("command"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("command"), rdfslabel, "Command")
+
+    # Status property on Observation.
+    add_uri(obs, prop_uri("status"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("status"), rdfslabel, "Status")
+
+    # URL property on Observation.
+    add_uri(obs, prop_uri("url"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("url"), rdfslabel, "URL")
+
+    # Time property on Observation.
+    add_uri(obs, prop_uri("time"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("time"), rdfslabel, "Time")
+
+    # Country property on Observation.
+    add_uri(obs, prop_uri("country"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("country"), rdfslabel, "Country of origin")
+
+    # Message type property on Observation
+    add_uri(obs, prop_uri("type"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("type"), rdfslabel, "Type")
+
+    # DNS Query on Observation
+    add_uri(obs, prop_uri("query"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("query"), rdfslabel, "DNS Query")
+
+    # DNS Answer (name) on Observation
+    add_uri(obs, prop_uri("answer_name"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("answer_name"), rdfslabel, "Answer (name)")
+
+    # DNS Query on Observation
+    add_uri(obs, prop_uri("answer_address"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("answer_address"), rdfslabel, "Answer (address)")
+
+    # Protocol context
+    add_uri(obs, prop_uri("context"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("context"), rdfslabel, "Context")
+
+    # From property on Observation
+    add_uri(obs, prop_uri("from"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("from"), rdfslabel, "From")
+
+    # To property on Observation
+    add_uri(obs, prop_uri("to"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("to"), rdfslabel, "To")
+
+    # Source property on Observation
+    add_uri(obs, prop_uri("source"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("source"), rdfslabel, "Source address")
+
+    # Destination property on Observation
+    add_uri(obs, prop_uri("destination"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("destination"), rdfslabel, "Destination address")
+
+    # IP type
+    add_uri(obs, type_uri("ip"), rdftype, rdf + "Resource")
+    add_string(obs, type_uri("ip"), rdfslabel, "IP address")
+
+    # TCP type
+    add_uri(obs, type_uri("tcp"), rdftype, rdf + "Resource")
+    add_string(obs, type_uri("tcp"), rdfslabel, "TCP port")
+
+    # UDP type
+    add_uri(obs, type_uri("udp"), rdftype, rdf + "Resource")
+    add_string(obs, type_uri("udp"), rdfslabel, "UDP port")
+
+    output(obs)
+
+    # Port property on TCP and UDP
+    add_uri(obs, prop_uri("port"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("port"), rdfslabel, "Port")
+
+    # IP property on IP, TCP and UDP
+    add_uri(obs, prop_uri("ip"), rdftype, rdf + "Property")
+    add_string(obs, prop_uri("ip"), rdfslabel, "IP")
+
+def output(obs):
+    for v in obs:
+        print "%-75s %-65s %s" % (v[0], v[1], v[2])
+    print
+
+############################################################################
+
+def handle(msg):
+
+    id = str(uuid.uuid1())
+    obs = []
+
+    uri = obj_uri("obs", id)
+    add_string(obs, uri, rdftype, type_uri("observation"))
+    
+    add_string(obs, uri, prop_uri("action"), msg["action"])
+    add_uri(obs, uri, prop_uri("device"), obj_uri("device", msg["device"]))
+
+    add_uri(obs, obj_uri("device", msg["device"]), rdftype, type_uri("device"))
+    add_string(obs, obj_uri("device", msg["device"]), rdfslabel,
+               "Device " + msg["device"])
+
+    add_dt(obs, uri, prop_uri("time"), msg["time"])
+    if msg.has_key("method"):
+        add_string(obs, uri, prop_uri("method"), msg["method"])
+    if msg.has_key("url"):
+        add_uri(obs, uri, prop_uri("url"), msg["url"])
+    if msg.has_key("command"):
+        add_string(obs, uri, prop_uri("command"), msg["command"])
+    if msg.has_key("status"):
+        add_string(obs, uri, prop_uri("status"), msg["status"])
+    if msg.has_key("text"):
+        t = ""
+        s = ""
+        for v in msg["text"]:
+            t = t + v + s
+            s = " "
+        add_string(obs, uri, prop_uri("text"), t)
+    if msg.has_key("payload"):
+        pass
+    if msg.has_key("body"):
+        pass
+    if msg.has_key("from"):
+        add_string(obs, uri, prop_uri("from"), msg["from"])
+    if msg.has_key("to"):
+        for v in msg["to"]:
+            add_string(obs, uri, prop_uri("to"), v)
+    if msg.has_key("header"):
+        for k in msg["header"]:
+            add_string(obs, uri, prop_uri("header:" + k), msg["header"][k])
+    if msg.has_key("type"):
+        add_string(obs, uri, prop_uri("type"), msg["type"])
+    if msg.has_key("queries"):
+        for v in msg["queries"]:
+            add_string(obs, uri, prop_uri("query"), v)
+    if msg.has_key("answers"):
+        for v in msg["answers"]:
+            if v.has_key("name"):
+                add_string(obs, uri, prop_uri("answer_name"), v["name"])
+            if v.has_key("address"):
+                add_string(obs, uri, prop_uri("answer_address"), v["address"])
+
+    if msg.has_key("src"):
+        ip = None
+        for v in msg["src"]:
+            if v.find(":") < 0:
+                cls = v
+                addr = ""
+            else:
+                cls = v[0:v.find(":")]
+                addr = v[v.find(":") + 1:]
+
+            if cls == "tcp":
+                fulladdr = ip + ":" + addr
+                add_uri(obs, uri, prop_uri("src"), obj_uri("tcp", fulladdr))
+                add_uri(obs, obj_uri("tcp", fulladdr), rdftype, type_uri("tcp"))
+                add_string(obs, obj_uri("tcp", fulladdr), rdfslabel,
+                           "TCP " + fulladdr)
+                add_uri(obs, obj_uri("tcp", fulladdr), prop_uri("context"),
+                        obj_uri("ip", ip))
+                add_string(obs, obj_uri("tcp", fulladdr), prop_uri("ip"), ip)
+                add_string(obs, obj_uri("tcp", fulladdr), prop_uri("port"),
+                           addr)
+
+            if cls == "udp":
+                fulladdr = ip + ":" + addr
+                add_uri(obs, uri, prop_uri("src"), obj_uri("udp", fulladdr))
+                add_uri(obs, obj_uri("udp", fulladdr), rdftype, type_uri("udp"))
+                add_string(obs, obj_uri("udp", fulladdr), rdfslabel,
+                           "UDP " + fulladdr)
+                add_uri(obs, obj_uri("udp", fulladdr), prop_uri("context"),
+                        obj_uri("ip", ip))
+                add_string(obs, obj_uri("udp", fulladdr), prop_uri("ip"), ip)
+                add_string(obs, obj_uri("udp", fulladdr), prop_uri("port"),
+                           addr)
+
+            if cls == "ipv4":
+                ip = addr
+                add_uri(obs, obj_uri("ip", addr), rdftype, type_uri("ip"))
+                add_string(obs, obj_uri("ip", addr), rdfslabel, addr)
+                add_string(obs, obj_uri("ip", addr), prop_uri("ip"), addr)
+
+    if msg.has_key("dest"):
+        ip = None
+        for v in msg["dest"]:
+            if v.find(":") < 0:
+                cls = v
+                addr = ""
+            else:
+                cls = v[0:v.find(":")]
+                addr = v[v.find(":") + 1:]
+
+            if cls == "tcp":
+                fulladdr = ip + ":" + addr
+                add_uri(obs, uri, prop_uri("dest"), obj_uri("tcp", fulladdr))
+                add_uri(obs, obj_uri("tcp", fulladdr), rdftype, type_uri("tcp"))
+                add_string(obs, obj_uri("tcp", fulladdr), rdfslabel,
+                           "TCP " + fulladdr)
+                add_uri(obs, obj_uri("tcp", fulladdr), prop_uri("context"),
+                        obj_uri("ip", ip))
+                add_string(obs, obj_uri("tcp", fulladdr), prop_uri("ip"), ip)
+                add_string(obs, obj_uri("tcp", fulladdr), prop_uri("port"),
+                           addr)
+
+            if cls == "udp":
+                fulladdr = ip + ":" + addr
+                add_uri(obs, uri, prop_uri("dest"), obj_uri("udp", fulladdr))
+                add_uri(obs, obj_uri("udp", fulladdr), rdftype, type_uri("udp"))
+                add_string(obs, obj_uri("udp", fulladdr), rdfslabel,
+                           "UDP " + fulladdr)
+                add_uri(obs, obj_uri("udp", fulladdr), prop_uri("context"),
+                        obj_uri("ip", ip))
+                add_string(obs, obj_uri("udp", fulladdr), prop_uri("ip"), ip)
+                add_string(obs, obj_uri("udp", fulladdr), prop_uri("port"),
+                           addr)
+
+            if cls == "ipv4":
+                ip = addr
+                add_uri(obs, obj_uri("ip", addr), rdftype, type_uri("ip"))
+                add_string(obs, obj_uri("ip", addr), rdfslabel, addr)
+                add_string(obs, obj_uri("ip", addr), prop_uri("ip"), addr)
+
+    output(obs)
+
+    sys.exit(0)
+
+############################################################################
+
+ctxt = zmq.Context()
+skt = ctxt.socket(zmq.SUB)
+skt.connect(binding)
+skt.setsockopt(zmq.SUBSCRIBE, "")
+
+init()
+
+while True:
+    msg = skt.recv()
+    handle(json.loads(msg))
+
--- docs/subscriber.py
+++ docs/subscriber.py
@@ -1,138 +1,54 @@
 #!/usr/bin/env python
 
+import zmq
 import json
-import time
-import base64
-import hmac
-import Crypto.Hash.SHA256 as SHA256
-import requests
-import Crypto.Signature.PKCS1_v1_5 as PKCS1_v1_5
-import Crypto.PublicKey.RSA as RSA
-import sys
 
-s = requests.Session()
+def handle(msg):
+    print
+    print "Action: %s" % msg["action"]
+    print "Device: %s" % msg["device"]
+    print "Time: %s" % msg["time"]
+    if msg.has_key("method"):
+        print "Method: %s" % msg["method"]
+    if msg.has_key("url"):
+        print "URL: %s" % msg["url"]
+    if msg.has_key("command"):
+        print "Command: %s" % msg["command"]
+    if msg.has_key("status"):
+        print "Status: %s" % msg["status"]
+    if msg.has_key("text"):
+        for v in msg["text"]:
+            print "Text: %s" % v
+    if msg.has_key("payload"):
+        print "Payload: <present>"
+    if msg.has_key("body"):
+        print "Body: <present>"
+    if msg.has_key("from"):
+        print "From: %s" % msg["from"]
+    if msg.has_key("to"):
+        for v in msg["to"]:
+            print "To: %s" % v
+    if msg.has_key("header"):
+        for k in msg["header"]:
+            print "%s: %s" % (k, msg["header"][k])
+    if msg.has_key("type"):
+        print "Type: %s" % msg["type"]
+    if msg.has_key("queries"):
+        for v in msg["queries"]:
+            print "Query: %s" % v
+    if msg.has_key("answers"):
+        for v in msg["answers"]:
+            if v.has_key("name"):
+                print "Answer name: %s" % v["name"]
+            if v.has_key("address"):
+                print "Answer address: %s" % v["address"]
 
-private = json.loads(open("private.json", "r").read())
-email = private["client_email"]
-private = private["private_key"]
-private = RSA.importKey(private)
-
-header = { 'alg': 'RS256', 'typ': 'JWT'}
-header = json.dumps(header)
-header = base64.urlsafe_b64encode(header)
-
-# Create an PKCS1_v1_5 object
-signer = PKCS1_v1_5.new(private)
-
-cs = {
-    "iss": email,
-    "scope": "https://www.googleapis.com/auth/cloud-platform",
-    "aud": "https://www.googleapis.com/oauth2/v4/token",
-    "exp": int(time.time() + 3600),
-    "iat": int(time.time())
-    }
-cs = json.dumps(cs)
-cs = base64.b64encode(cs)
-
-msg_hash = SHA256.new(header + "." + cs)
-signature = signer.sign(msg_hash)
-signature = base64.urlsafe_b64encode(signature)
-
-input = header + "." + cs
-
-jwt ="%s.%s.%s" % (header, cs, signature)
-
-gt = "urn:ietf:params:oauth:grant-type:jwt-bearer"
-data={'grant_type': gt, 'assertion': jwt}
-
-print "Authenticate..."
-uri = "https://www.googleapis.com/oauth2/v4/token"
-r = s.post(uri, data=data)
-response = r.json()
-if r.status_code != 200:
-    print "Failed"
-    print r.text
-    sys.exit(0)
-    
-token = response["access_token"]
-auth = "%s %s" % (response["token_type"], response["access_token"])
-
-project = "INSERT_PROJECT"
-topic = "cyberprobe"
-subs = "mysubs"
-
-# ------------------------------------------------------------
-
-subscription = {
-    "topic": "projects/%s/topics/%s" % (project, topic)
-}
-subscription=json.dumps(subscription)
-
-uri = "https://pubsub.googleapis.com/v1/projects/%s/subscriptions/%s" % \
-	(project, subs)
-
-print "Create subscription..."
-r = s.put(uri, headers={"Authorization": auth}, data=subscription)
-
-if r.status_code != 200 and r.status_code != 409:
-    print "Failed"
-    print r.text
-    sys.exit(0)
-
-# ------------------------------------------------------------
+ctxt = zmq.Context()
+skt = ctxt.socket(zmq.SUB)
+skt.connect("tcp://localhost:5555")
+skt.setsockopt(zmq.SUBSCRIBE, "")
 
 while True:
-
-    body = {
-        "returnImmediately": False,
-        "maxMessages": 100
-    }
-    body=json.dumps(body)
-
-    uri = "https://pubsub.googleapis.com/v1/projects/%s/subscriptions/%s:pull" % \
-	  (project, subs)
-    
-    r = s.post(uri, headers={"Authorization": auth}, data=body)
-    if r.status_code != 200:
-        print "Failed"
-        print r.text
-        sys.exit(0)
-
-    ids = []
-    obj = r.json()
-    if obj.has_key("receivedMessages"):
-        rm = obj["receivedMessages"]
-        for v in rm:
-            ids.append(v["ackId"])
-            msg = json.loads(base64.b64decode(v["message"]["data"]))
-            print msg["action"]
-
-        body = {
-            "ackIds": ids
-        }
-        body = json.dumps(body)
-        uri = "https://pubsub.googleapis.com/v1/projects/%s/subscriptions/%s:acknowledge" % \
-	      (project, subs)
-
-        r = s.post(uri, headers={"Authorization": auth}, data=body)
-        if r.status_code != 200:
-            print "Failed"
-            print r.text
-            sys.exit(0)
-
-sys.exit(0)
-
-# ------------------------------------------------------------
-
-uri = "https://pubsub.googleapis.com/v1/projects/%s/subscriptions/%s" % \
-	(project, subs)
-
-print "Delete subscription..."
-r = s.delete(uri, headers={"Authorization": auth})
-if r.status_code != 200:
-    print "Failed"
-    print r.text
-    sys.exit(0)
-
-# ------------------------------------------------------------
+    msg = skt.recv()
+    handle(json.loads(msg))
 
