6c36a1d7f7adea88b0ee66d32eaa7a9ba98ecd1a
Cyber MacGeddon <cybermaggedon@gmail.com>
2016-08-08 21:47:18 +0100

    Remove check from package build dist - the package build does distcheck anyway

--- pkg.mk
+++ pkg.mk
@@ -5,7 +5,7 @@ deb:
 	rm -rf cyberprobe-${VERSION}
 	autoreconf -fi
 	./configure
-	make distcheck
+	make dist
 	cp cyberprobe-${VERSION}.tar.gz cyberprobe_${VERSION}.orig.tar.gz
 	tar xvfz cyberprobe-${VERSION}.tar.gz
 	cp -r debian/ cyberprobe-${VERSION}/
@@ -14,7 +14,7 @@ deb:
 rpm:
 	autoreconf -fi
 	./configure
-	make distcheck
+	make dist
 	mkdir -p RPM/SOURCES
 	cp cyberprobe-${VERSION}.tar.gz RPM/SOURCES/
 	rpmbuild --define "_topdir `pwd`/RPM" -ba cyberprobe.spec
