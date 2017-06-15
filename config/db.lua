4dbf96c4536725d2285f36e6d33bbeaca205adae
Andy Hassall <andy.hassall@trustnetworks.com>
2017-06-15 14:31:34 +0100

    refactor db.lua and zeromq.lua to use same json creation

--- config/db.lua
+++ config/db.lua
@@ -10,255 +10,46 @@
 -- returned to the calling environment.  It doesn't matter what you call it.
 local observer = {}
 
-local mime = require("mime")
 local elastic = require("util.elastic")
+local model = require("util.json")
 
-local b64 = function(x)
-  local a, b = mime.b64(x)
-  return a
-end
-
--- Elasticsearch init
 elastic.init()
 
--- The table should contain functions.
-
--- This function is called when a trigger events starts collection of an
--- attacker. liid=the trigger ID, addr=trigger address
-observer.trigger_up = function(liid, addr)
+-- ZeroMQ object submission function - just pushes the object onto the queue.
+local submit = function(obs)
+  ret = elastic.submit_observation(obs)
 end
 
--- This function is called when an attacker goes off the air
-observer.trigger_down = function(liid)
-end
+-- Call the JSON functions for all observer functions.
+observer.trigger_up = model.trigger_up
+observer.trigger_down = model.trigger_down
+observer.connection_up = model.connection_up
+observer.connection_down = model.connection_down
+observer.unrecognised_datagram = model.unrecognised_datagram
+observer.unrecognised_stream = model.unrecognised_stream
+observer.icmp = model.icmp
+observer.imap = model.imap
+observer.imap_ssl = model.imap_ssl
+observer.pop3 = model.pop3
+observer.pop3_ssl = model.pop3_ssl
+observer.http_request = model.http_request
+observer.http_response = model.http_response
+observer.sip_request = model.sip_request
+observer.sip_response = model.sip_response
+observer.sip_ssl = model.sip_ssl
+observer.smtp_command = model.smtp_command
+observer.smtp_response = model.smtp_response
+observer.smtp_data = model.smtp_data
+observer.dns_message = model.dns_message
+observer.ftp_command = model.ftp_command
+observer.ftp_response = model.ftp_response
+observer.ntp_timestamp_message = model.ntp_timestamp_message
+observer.ntp_control_message = model.ntp_control_message
+observer.ntp_private_message = model.ntp_private_message
 
--- This function is called when a stream-orientated connection is made
--- (e.g. TCP)
-observer.connection_up = function(context)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "connected_up"
-  elastic.submit_observation(obs)
-end
+-- Register Redis submission.
+model.init(submit)
 
--- This function is called when a stream-orientated connection is closed
-observer.connection_down = function(context)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "connected_down"
-  elastic.submit_observation(obs)
-end
-
--- This function is called when a datagram is observed, but the protocol
--- is not recognised.
-observer.unrecognised_datagram = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "unrecognised_datagram"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when stream data  is observed, but the protocol
--- is not recognised.
-observer.unrecognised_stream = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "unrecognised_stream"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an ICMP message is observed.
-observer.icmp = function(context, icmp_type, icmp_code, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "icmp"
-  obs["observation"]["type"] = icmp_type
-  obs["observation"]["code"] = icmp_code
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an IMAP message is observed.
-observer.imap = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "imap"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an IMAP SSL message is observed.
-observer.imap_ssl = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "imap_ssl"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when a POP3 message is observed.
-observer.pop3 = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "pop3"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when a POP3 SSL message is observed.
-observer.pop3_ssl = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "pop3_ssl"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when a SIP request is observed.
-observer.sip_request = function(context, method,from, to, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "sip_request"
-  obs["observation"]["method"] = method
-  obs["observation"]["from"] = from
-  obs["observation"]["to"] = to
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when a SIP response is observed.
-observer.sip_response = function(context, code, status, from, to, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "sip_response"
-  obs["observation"]["code"] = code
-  obs["observation"]["status"] = status
-  obs["observation"]["from"] = from
-  obs["observation"]["to"] = to
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when a SIP SSL message is observed.
-observer.sip_ssl = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "pop3_ssl"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an SMTP command is observed.
-observer.smtp_command = function(context, command)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "smtp_command"
-  obs["observation"]["command"] = command
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an SMTP response is observed.
-observer.smtp_response = function(context, status, text)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "smtp_response"
-  obs["observation"]["status"] = status
-  obs["observation"]["text"] = text
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an SMTP response is observed.
-observer.smtp_data = function(context, from, to, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "smtp_data"
-  obs["observation"]["from"] = from
-  obs["observation"]["to"] = to
-  obs["observation"]["body"] = data
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an SMTP Authentication message is observed.
-observer.smtp_auth = function(context, data)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "smtp_auth"
-  obs["observation"]["data"] = b64(data)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an HTTP request is observed.
-observer.http_request = function(context, method, url, header, body)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "http_request"
-  obs["observation"]["method"] = method
-  obs["observation"]["url"] = url
-  obs["observation"]["header"] = header
-  obs["observation"]["body"] = b64(body)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an HTTP response is observed.
-observer.http_response = function(context, code, status, header, url, body)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "http_response"
-  obs["observation"]["code"] = code
-  obs["observation"]["status"] = status
-  obs["observation"]["header"] = header
-  obs["observation"]["url"] = url
-  obs["observation"]["body"] = b64(body)
-  elastic.submit_observation(obs)
-end
-
--- This function is called when a DNS message is observed.
-observer.dns_message = function(context, header, queries, answers, auth, add)
-  local obs = elastic.initialise_observation(context)
-
-  obs["observation"]["action"] = "dns_message"
-
-  if header.qr == 0 then
-    obs["observation"]["type"] = "query"
-  else
-    obs["observation"]["type"] = "response"
-  end
-
-  local q = {}
-  for key, value in pairs(queries) do
-    q[#q + 1] = value.name
-  end
-  obs["observation"]["queries"] = q
-
-  q = {}
-  for key, value in pairs(answers) do
-    local a = {}
-    a["name"] = value.name
-    if value.rdaddress then
-       a["address"] = value.rdaddress
-    end
-    if value.rdname then
-       a["name"] = value.rdname
-    end
-    q[#q + 1] = a
-  end
-  obs["observation"]["answers"] = q
-  elastic.submit_observation(obs)
-end
-
-
--- This function is called when an FTP command is observed.
-observer.ftp_command = function(context, command)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "ftp_command"
-  obs["observation"]["command"] = command
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an FTP response is observed.
-observer.ftp_response = function(context, status, text)
-  local obs = elastic.initialise_observation(context)
-  obs["observation"]["action"] = "ftp_response"
-  obs["observation"]["status"] = status
-  obs["observation"]["text"] = text
-  elastic.submit_observation(obs)
-end
-
--- This function is called when an NTP timestamp message is observed.
-observer.ntp_timestamp_message = function(context, hdr, info)
-end
-
--- This function is called when an NTP control message is observed.
-observer.ntp_control_message = function(context, hdr, info)
-end
-
--- This function is called when an NTP private message is observed.
-observer.ntp_private_message = function(context, hdr, info)
-end
 
 -- Return the table
 return observer
