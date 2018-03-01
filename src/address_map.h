b446d8cf655170d5cde38e52387a905ad2330e10
Cyber MacGeddon <cybermaggedon@gmail.com>
2018-03-01 18:15:09 +0000

    [ch2901] Added documentation for the '%' template constructs, and documented address_map.h.

--- src/address_map.h
+++ src/address_map.h
@@ -1,6 +1,30 @@
 
+// Longest-prefix IP address matching.
+//
+// This header provides a template class address_map<A, T> which
+// is used to map addresses of type A to values of type T.  Address masks
+// are used, so that an address of value 1.2.9.12 will match a key of
+// 1.2.0.0/16.  The longest prefix i.e. most specific address always matches
+// first.
+//
+// The only requirement on A is that it supports the '&' operation so that
+// it provides operator&(unsigned int mask) such that for an address of type
+// A, and an unsigned integer value m, A&m returns an address of type
+// A containing only the first m bits of the address.  All other bits are zered.
+
+// e.g.
+//   tcpip::ip4_address addr1("15.12.8.1");
+//   tcpip::ip4_address addr2 = addr1 & 16;
+//   std::string str;
+//   addr2.to_string(str);
+//   assert(str == "15.12.0.0");
+//
+
 #include <map>
 #include <iostream>
+
+#ifndef ADDRESS_MAP_H
+#define ADDRESS_MAP_H
 
 template <class A, class T>
 class address_map {
@@ -15,20 +39,24 @@ public:
 
 public:
 
+    // Adds a key to the map, address 'a', mask 'mask', value 't'.
     void insert(const A& a, unsigned int mask, T t) {
 	m[mask][a & mask] = t;
     }
 
+    // Removes a key from the map, address 'a', mask 'mark'.
     void remove(A a, unsigned int mask) {
 	m[mask].erase(a & mask);
     }
 
+    // Searches the map for address 'a'.  If it exists, returns true and
+    // a pointer to the value is returned in 't'.  Otherwise, returns false,
+    // and t is undefined.
     bool get(const A& a, T*& t) {
       
 	typename mask_map::reverse_iterator it;
 
 	for(it = m.rbegin(); it != m.rend(); it++) {
-
 
 	    typename std::map<A, T>::iterator it2;
 
@@ -53,4 +81,6 @@ public:
     }
 
 };
+
+#endif
 
