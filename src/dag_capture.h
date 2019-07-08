bef57bae144c2664bf88eca37fde7ad3cb4ab3be
Mark Adams <cybermaggedon@gmail.com>
2019-07-08 09:20:40 -0700

    Got DAG code compiling with new API, not tested

--- src/dag_capture.h
+++ src/dag_capture.h
@@ -10,36 +10,18 @@
 
 // Packet capture.  Captures on an interface, and then submits captured
 // packets to the delivery engine.
-class dag_dev : public capture_dev {
+class dag_dev : public delayline_dev {
 private:
 
-    struct delayed_packet {
-	std::vector<unsigned char> packet;
-	struct timeval exit_time;
-    };
-
     std::string iface;
-
-    std::queue<delayed_packet> delay_line;
-
-    // Handle to the deliver engine.
-    packet_consumer& deliv;
-
-    // Seconds of delay
-    float delay;
-
-    // Delay converted to timeval form.
-    struct timeval delay_val;
-
-    // PCAP's datalink enumerator - describes the type of layer 2 wrapping
-    // on the IP packet.
-    int datalink;
 
     // Set to false to stop.
     bool running;
 
     // Packet filter.
     std::string filter;
+
+    std::thread* thr;
 
 public:
 
@@ -48,9 +30,7 @@ public:
 
     // Constructor.  i=interface name, d=packet consumer.
     dag_dev(const std::string& i, float delay, packet_consumer& d) :
-        deliv(d) {
-        this->delay = delay;
-        this->datalink = DLT_EN10MB; // FIXME: Hard-coded?
+        delayline_dev(d, delay, DLT_EN10MB) {
         this->iface = i;
         this->running = true;
     }
@@ -58,16 +38,21 @@ public:
     // Destructor.
     virtual ~dag_dev() {}
 
-    // Packet handler.
-    virtual void handle(timeval tv, unsigned long len, unsigned long captured,
-			const unsigned char* bytes);
-
     virtual void stop() {
 	running = false;
     }
 
     void add_filter(const std::string& spec) {
 	filter = spec;
+    }
+
+    virtual void join() {
+	if (thr)
+	    thr->join();
+    }
+    
+    virtual void start() {
+	thr = new std::thread(&dag_dev::run, this);
     }
 
 };
