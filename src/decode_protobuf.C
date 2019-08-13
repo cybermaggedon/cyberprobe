fb7d0affb794ddfbfb1cc5af3bb8e9853ca39888
Mark Adams <cybermaggedon@gmail.com>
2019-08-13 09:05:41 -0700

    Tidy code a bit

--- src/decode_protobuf.C
+++ src/decode_protobuf.C
@@ -21,43 +21,140 @@ using google::protobuf::util::TimeUtil;
 using grpc::ServerCompletionQueue;
 using grpc::ServerAsyncResponseWriter;
 
+const int indent = 15;
+
+void display(const cyberprobe::Event& ev) {
+
+                
+    std::cout << std::endl;
+    std::cout << std::setw(indent) << std::left
+              << "Id: " << ev.id() << std::endl;
+    std::cout << std::setw(indent) << std::left
+              << "Time: " << TimeUtil::ToString(ev.time()) << std::endl;
+    std::cout << std::setw(indent) << std::left
+              << "Action: " << Action_Name(ev.action())
+              << std::endl;
+    std::cout << std::setw(indent) << std::left
+              << "Device: " << ev.device()
+              << std::endl;
+    if (ev.network() != "")
+        std::cout << std::setw(indent) << std::left
+                  << "Network: " << ev.network()
+                  << std::endl;
+    if (ev.origin() == cyberprobe::Origin::network)
+        std::cout << std::setw(indent) << std::left
+                  << "Origin: " << "network"
+                  << std::endl;
+    else if (ev.origin() == cyberprobe::Origin::device)
+        std::cout << std::setw(indent) << std::left
+                  << "Origin: " << "device"
+                  << std::endl;
+
+    std::cout << std::setw(indent) << std::left << "Src: ";
+    for(auto it = ev.src().begin();
+        it != ev.src().end();
+        it++) {
+        std::cout << cyberprobe::Protocol_Name(it->protocol());
+
+        auto a = it->address();
+
+        if (a.address_variant_case() == cyberprobe::Address::kIpv4) {
+            std::cout << ":"
+                      << ((it->address().ipv4() >> 24) & 0xff) << "."
+                      << ((it->address().ipv4() >> 16) & 0xff) << "."
+                      << ((it->address().ipv4() >> 8) & 0xff) << "."
+                      << (it->address().ipv4() & 0xff);
+        }
+
+        if (a.address_variant_case() == cyberprobe::Address::kIpv6) {
+            tcpip::ip6_address ip;
+            ip.addr.assign(it->address().ipv6().begin(),
+                           it->address().ipv6().end());
+            std::string a;
+            ip.to_string(a);
+            std::cout << ":" << a;
+        }
+
+        if (a.address_variant_case() == cyberprobe::Address::kPort) {
+            std::cout << ":" << it->address().port();
+        }
+
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << std::setw(indent) << std::left << "Dest: ";
+    for(auto it = ev.dest().begin();
+        it != ev.dest().end();
+        it++) {
+        std::cout << cyberprobe::Protocol_Name(it->protocol());
+
+        auto a = it->address();
+
+        if (a.address_variant_case() == cyberprobe::Address::kIpv4) {
+            std::cout << ":"
+                      << ((it->address().ipv4() >> 24) & 0xff) << "."
+                      << ((it->address().ipv4() >> 16) & 0xff) << "."
+                      << ((it->address().ipv4() >> 8) & 0xff) << "."
+                      << (it->address().ipv4() & 0xff);
+        }
+
+        if (a.address_variant_case() == cyberprobe::Address::kIpv6) {
+            tcpip::ip6_address ip;
+            ip.addr.assign(it->address().ipv6().begin(),
+                           it->address().ipv6().end());
+            std::string a;
+            ip.to_string(a);
+            std::cout << ":" << a;
+        }
+
+        if (a.address_variant_case() == cyberprobe::Address::kPort) {
+            std::cout << ":" << it->address().port();
+        }
+
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+
+}
+
 class ServerImpl final {
 public:
     ~ServerImpl() {
 
-        server_->Shutdown();
+        server->Shutdown();
 
         // Always shutdown the completion queue after the server.
-        cq_->Shutdown();
+        cq->Shutdown();
 
     }
 
     // There is no shutdown handling in this code.
     void Run() {
 
-        std::string server_address("0.0.0.0:50051");
+        std::string serveraddress("0.0.0.0:50051");
 
         ServerBuilder builder;
 
         // Listen on the given address without any authentication mechanism.
-        builder.AddListeningPort(server_address,
+        builder.AddListeningPort(serveraddress,
                                  grpc::InsecureServerCredentials());
 
-        // Register "service_" as the instance through which we'll
+        // Register "service" as the instance through which we'll
         // communicate with clients. In this case it corresponds to an
         // *asynchronous* service.
-        builder.RegisterService(&service_);
+        builder.RegisterService(&service);
 
         // Get hold of the completion queue used for the asynchronous
         // communication with the gRPC runtime.
-        cq_ = builder.AddCompletionQueue();
+        cq = builder.AddCompletionQueue();
 
         // Finally assemble the server.
-        server_ = builder.BuildAndStart();
-        std::cout << "Server listening on " << server_address << std::endl;
+        server = builder.BuildAndStart();
+        std::cout << "Server listening on " << serveraddress << std::endl;
 
         // Proceed to the server's main loop.
-        HandleRpcs();
+        handle_rpcs();
     }
 
 private:
@@ -71,49 +168,41 @@ private:
         // asynchronous communication with the gRPC runtime.
         CallData(cyberprobe::EventStream::AsyncService* service,
                  ServerCompletionQueue* cq)
-            : service_(service), cq_(cq), responder_(&ctx_), status_(CREATE) {
+            : service(service), cq(cq), responder(&ctx), status(CREATE) {
             // Invoke the serving logic right away.
             Proceed();
         }
 
         void Proceed() {
 
-            if (status_ == CREATE) {
+            if (status == CREATE) {
 
                 // Make this instance progress to the PROCESS state.
-                status_ = PROCESS;
+                status = PROCESS;
 
                 // As part of the initial CREATE state, we *request* that the system
                 // start processing SayHello requests. In this request, "this" acts are
                 // the tag uniquely identifying the request (so that different CallData
                 // instances can serve different requests concurrently), in this case
                 // the memory address of this CallData instance.
-                service_->RequestObserve(&ctx_, &request_, &responder_, cq_,
-                                         cq_, this);
+                service->RequestObserve(&ctx, &request, &responder, cq,
+                                        cq, this);
 
-            } else if (status_ == PROCESS) {
+            } else if (status == PROCESS) {
 
                 // Spawn a new CallData instance to serve new clients while we process
                 // the one for this CallData. The instance will deallocate itself as
                 // part of its FINISH state.
-                new CallData(service_, cq_);
+                new CallData(service, cq);
 
-
-std::cout << std::setw(30) << std::left
-<< "Id: " << request_.id() << std::endl;
-
-                // The actual processing.
-                // DO nothing.
-//                std::string prefix("Hello ");
-//                reply_.set_message(prefix + request_.name());
+                display(request);
 
                 // And we are done! Let the gRPC runtime know we've finished, using the
                 // memory address of this instance as the uniquely identifying tag for
                 // the event.
-                status_ = FINISH;
-                responder_.Finish(reply_, Status::OK, this);
+                status = FINISH;
+                responder.Finish(reply, Status::OK, this);
             } else {
-//                GPR_ASSERT(status_ == FINISH);
                 // Once in the FINISH state, deallocate ourselves (CallData).
                 delete this;
             }
@@ -122,31 +211,31 @@ std::cout << std::setw(30) << std::left
     private:
         // The means of communication with the gRPC runtime for an asynchronous
         // server.
-        cyberprobe::EventStream::AsyncService* service_;
+        cyberprobe::EventStream::AsyncService* service;
         // The producer-consumer queue where for asynchronous server notifications.
-        ServerCompletionQueue* cq_;
+        ServerCompletionQueue* cq;
         // Context for the rpc, allowing to tweak aspects of it such as the use
         // of compression, authentication, as well as to send metadata back to the
         // client.
-        ServerContext ctx_;
+        ServerContext ctx;
 
         // What we get from the client.
-        Event request_;
+        Event request;
         // What we send back to the client.
-        Empty reply_;
+        Empty reply;
 
         // The means to get back to the client.
-        ServerAsyncResponseWriter<Empty> responder_;
+        ServerAsyncResponseWriter<Empty> responder;
 
         // Let's implement a tiny state machine with the following states.
         enum CallStatus { CREATE, PROCESS, FINISH };
-        CallStatus status_;  // The current serving state.
+        CallStatus status;  // The current serving state.
     };
 
     // This can be run in multiple threads if needed.
-    void HandleRpcs() {
+    void handle_rpcs() {
         // Spawn a new CallData instance to serve new clients.
-        new CallData(&service_, cq_.get());
+        new CallData(&service, cq.get());
         void* tag;  // uniquely identifies a request.
         bool ok;
         while (true) {
@@ -154,16 +243,15 @@ std::cout << std::setw(30) << std::left
             // event is uniquely identified by its tag, which in this case is the
             // memory address of a CallData instance.
             // The return value of Next should always be checked. This return value
-            // tells us whether there is any kind of event or cq_ is shutting down.
-            GPR_ASSERT(cq_->Next(&tag, &ok));
-//            GPR_ASSERT(ok);
+            // tells us whether there is any kind of event or cq is shutting down.
+            GPR_ASSERT(cq->Next(&tag, &ok));
             static_cast<CallData*>(tag)->Proceed();
         }
     }
 
-    std::unique_ptr<ServerCompletionQueue> cq_;
-    cyberprobe::EventStream::AsyncService service_;
-    std::unique_ptr<Server> server_;
+    std::unique_ptr<ServerCompletionQueue> cq;
+    cyberprobe::EventStream::AsyncService service;
+    std::unique_ptr<Server> server;
 };
 
 int main(int argc, char** argv) {
@@ -173,146 +261,3 @@ int main(int argc, char** argv) {
     return 0;
 }
 
-
-
-
-
-
-/*
-// Logic and data behind the server's behavior.
-class EventStreamServiceImpl final : public EventStream::Service {
-
-Status Observe(ServerContext* context, const Event* request,
-Empty* reply) override {
-
-std::cout << std::endl;
-std::cout << std::setw(30) << std::left
-<< "Id: " << request->id() << std::endl;
-std::cout << std::setw(30) << std::left
-<< "Time: " << TimeUtil::ToString(request->time()) << std::endl;
-std::cout << std::setw(30) << std::left
-<< "Action: " << Action_Name(request->action())
-<< std::endl;
-std::cout << std::setw(30) << std::left
-<< "Device: " << request->device()
-<< std::endl;
-if (request->network() != "")
-std::cout << std::setw(30) << std::left
-<< "Network: " << request->network()
-<< std::endl;
-if (request->origin() == cyberprobe::Origin::network)
-std::cout << std::setw(30) << std::left
-<< "Origin: " << "network"
-<< std::endl;
-else if (request->origin() == cyberprobe::Origin::device)
-std::cout << std::setw(30) << std::left
-<< "Origin: " << "device"
-<< std::endl;
-
-std::cout << std::setw(30) << std::left << "Src: ";
-for(auto it = request->src().begin();
-it != request->src().end();
-it++) {
-std::cout << cyberprobe::Protocol_Name(it->protocol());
-
-auto a = it->address();
-
-if (a.address_variant_case() == cyberprobe::Address::kIpv4) {
-std::cout << ":"
-<< ((it->address().ipv4() >> 24) & 0xff) << "."
-<< ((it->address().ipv4() >> 16) & 0xff) << "."
-<< ((it->address().ipv4() >> 8) & 0xff) << "."
-<< (it->address().ipv4() & 0xff);
-}
-
-if (a.address_variant_case() == cyberprobe::Address::kIpv6) {
-tcpip::ip6_address ip;
-ip.addr.assign(it->address().ipv6().begin(),
-it->address().ipv6().end());
-std::string a;
-ip.to_string(a);
-std::cout << ":" << a;
-}
-
-if (a.address_variant_case() == cyberprobe::Address::kPort) {
-std::cout << ":" << it->address().port();
-}
-
-std::cout << " ";
-}
-std::cout << std::endl;
-
-std::cout << std::setw(30) << std::left << "Dest: ";
-for(auto it = request->dest().begin();
-it != request->dest().end();
-it++) {
-std::cout << cyberprobe::Protocol_Name(it->protocol());
-
-auto a = it->address();
-
-if (a.address_variant_case() == cyberprobe::Address::kIpv4) {
-std::cout << ":"
-<< ((it->address().ipv4() >> 24) & 0xff) << "."
-<< ((it->address().ipv4() >> 16) & 0xff) << "."
-<< ((it->address().ipv4() >> 8) & 0xff) << "."
-<< (it->address().ipv4() & 0xff);
-}
-
-if (a.address_variant_case() == cyberprobe::Address::kIpv6) {
-tcpip::ip6_address ip;
-ip.addr.assign(it->address().ipv6().begin(),
-it->address().ipv6().end());
-std::string a;
-ip.to_string(a);
-std::cout << ":" << a;
-}
-
-if (a.address_variant_case() == cyberprobe::Address::kPort) {
-std::cout << ":" << it->address().port();
-}
-
-std::cout << " ";
-}
-std::cout << std::endl;
-
-return Status::OK;
-}
-
-void run() {
-
-std::string server_address("0.0.0.0:50051");
-EventStreamServiceImpl service;
-
-ServerBuilder builder;
-
-// Listen on the given address without any authentication mechanism.
-builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
-
-// Register "service" as the instance through which we'll communicate with
-// clients. In this case it corresponds to an *synchronous* service.
-builder.RegisterService(&service);
-
-// Get hold of the completion queue used for the asynchronous communication
-// with the gRPC runtime.
-cq_ = builder.AddCompletionQueue();
-
-// Finally assemble the server.
-std::unique_ptr<Server> server(builder.BuildAndStart());
-std::cout << "Server listening on " << server_address << std::endl;
-
-// Wait for the server to shutdown. Note that some other thread must be
-// responsible for shutting down the server for this call to ever return.
-server->Wait();
-
-}
-    
-};
-
-int main(int argc, char** argv) {
-
-run();
-return 0;
-
-}
-
-*/
--- src/event_grpc.C
+++ src/event_grpc.C
@@ -26,143 +26,143 @@ using grpc::CompletionQueue;
 
 namespace cybermon {
 
-class eventstream_client {
+    class eventstream_client {
 
-  public:
-    explicit eventstream_client(std::shared_ptr<Channel> channel)
-        : stub_(cyberprobe::EventStream::NewStub(channel)), running(true) {}
+    public:
 
-    std::thread thr;
+        explicit eventstream_client(std::shared_ptr<Channel> channel)
+            : stub(cyberprobe::EventStream::NewStub(channel)), running(true) {}
 
-    void shutdown() {
-        running = false;
-        // FIXME: Shutdown completion queue here?
-        thr.join();
-    }
+        std::thread thr;
 
-    // Assembles the client's payload and sends it to the server.
-    void observe(std::shared_ptr<cybermon::event::event> ev) {
-        // Data we are sending to the server.
-        cyberprobe::Event request;
-        ev->to_protobuf(request);
-
-        // Call object to store rpc data
-        AsyncClientCall* call = new AsyncClientCall;
-
-        // stub_->PrepareAsyncSayHello() creates an RPC object, returning
-        // an instance to store in "call" but does not actually start the RPC
-        // Because we are using the asynchronous API, we need to hold on to
-        // the "call" instance in order to get updates on the ongoing RPC.
-        call->response_reader =
-            stub_->PrepareAsyncObserve(&call->context, request, &cq_);
-
-        // StartCall initiates the RPC call
-        call->response_reader->StartCall();
-
-        // Request that, upon completion of the RPC, "reply" be updated with the
-        // server's response; "status" with the indication of whether the operation
-        // was successful. Tag the request with the memory address of the call object.
-        call->response_reader->Finish(&call->reply, &call->status, (void*)call);
-
-    }
-
-    // Loop while listening for completed responses.
-    // Prints out the response from the server.
-    void AsyncCompleteRpc() {
-        void* got_tag;
-        bool ok = false;
-
-        std::cerr << "Async starting" << std::endl;
-
-        // Block until the next result is available in the completion queue "cq".
-        while (running && cq_.Next(&got_tag, &ok)) {
-
-            // The tag in this example is the memory location of the call object
-            AsyncClientCall* call = static_cast<AsyncClientCall*>(got_tag);
-
-            // Verify that the request was completed successfully. Note that "ok"
-            // corresponds solely to the request for updates introduced by Finish().
-            GPR_ASSERT(ok);
-
-            if (call->status.ok())
-                std::cout << "Client received." << std::endl;
-            else
-                std::cout << "RPC failed" << std::endl;
-
-            // Once we're complete, deallocate the call object.
-            delete call;
+        void shutdown() {
+            running = false;
+            // FIXME: Shutdown completion queue here?
+            thr.join();
         }
-    }
 
-    void start_async() {
-        thr = std::thread(&eventstream_client::AsyncCompleteRpc, this);
-    }
+        // Assembles the client's payload and sends it to the server.
+        void observe(std::shared_ptr<cybermon::event::event> ev) {
 
-  private:
+            // Data we are sending to the server.
+            cyberprobe::Event request;
+            ev->to_protobuf(request);
 
-    // struct for keeping state and data information
-    struct AsyncClientCall {
-        // Container for the data we expect from the server.
-        Empty reply;
+            // Call object to store rpc data
+            AsyncClientCall* call = new AsyncClientCall;
 
-        // Context for the client. It could be used to convey extra information to
-        // the server and/or tweak certain RPC behaviors.
-        ClientContext context;
+            // Create RPC object.
+            call->response_reader =
+                stub->PrepareAsyncObserve(&call->context, request, &cq);
 
-        // Storage for the status of the RPC upon completion.
-        Status status;
+            // StartCall initiates the RPC call
+            call->response_reader->StartCall();
 
+            // Request that, upon completion of the RPC, "reply" be updated with the
+            // server's response; "status" with the indication of whether the operation
+            // was successful. Tag the request with the memory address of the
+            // call object.
+            call->response_reader->Finish(&call->reply, &call->status, (void*)call);
 
-        std::unique_ptr<ClientAsyncResponseReader<Empty>> response_reader;
+        }
+
+        // Loop while listening for completed responses.
+        // Prints out the response from the server.
+        void async_complete() {
+
+            void* got_tag;
+            bool ok = false;
+
+            // Block until the next result is available in the completion queue "cq".
+            while (running && cq.Next(&got_tag, &ok)) {
+
+                // The tag in this example is the memory location of the call object
+                AsyncClientCall* call = static_cast<AsyncClientCall*>(got_tag);
+
+                // Verify that the request was completed successfully. Note that "ok"
+                // corresponds solely to the request for updates introduced by Finish().
+                GPR_ASSERT(ok);
+
+//            if (call->status.ok())
+//                std::cout << "Client received." << std::endl;
+//            else
+//                std::cout << "RPC failed" << std::endl;
+
+                // Once we're complete, deallocate the call object.
+                delete call;
+            }
+        }
+
+        void start_async() {
+            thr = std::thread(&eventstream_client::async_complete, this);
+        }
+
+    private:
+
+        // struct for keeping state and data information
+        struct AsyncClientCall {
+
+            // Container for the data we expect from the server.
+            Empty reply;
+
+            // Context for the client. It could be used to convey extra information to
+            // the server and/or tweak certain RPC behaviors.
+            ClientContext context;
+
+            // Storage for the status of the RPC upon completion.
+            Status status;
+
+            std::unique_ptr<ClientAsyncResponseReader<Empty>> response_reader;
+
+        };
+
+        // Out of the passed in Channel comes the stub, stored here, our view of the
+        // server's exposed services.
+        std::unique_ptr<cyberprobe::EventStream::Stub> stub;
+
+        // The producer-consumer queue we use to communicate asynchronously with the
+        // gRPC runtime.
+        CompletionQueue cq;
+    
+        bool running;
+
     };
 
-    // Out of the passed in Channel comes the stub, stored here, our view of the
-    // server's exposed services.
-    std::unique_ptr<cyberprobe::EventStream::Stub> stub_;
-
-    // The producer-consumer queue we use to communicate asynchronously with the
-    // gRPC runtime.
-    CompletionQueue cq_;
-    
-    bool running;
-
-};
-
-std::shared_ptr<grpc_manager> grpc_manager::create() {
-    return std::make_shared<grpc_manager>();
-}
-
-void grpc_manager::observe(std::shared_ptr<event::event> ev,
-                           const std::string& svc) {
-
-    if (client.count(svc) == 0) {
-
-        std::cerr << "Connecting gRPC to " << svc << std::endl;
-    
-        auto chan = grpc::CreateChannel(svc,
-                                        grpc::InsecureChannelCredentials());
-        auto cli = std::make_shared<eventstream_client>(chan);
-        client[svc] = cli;
-
-        cli->start_async();
-    
+    std::shared_ptr<grpc_manager> grpc_manager::create() {
+        return std::make_shared<grpc_manager>();
     }
 
-    client[svc]->observe(ev);
+    void grpc_manager::observe(std::shared_ptr<event::event> ev,
+                               const std::string& svc) {
 
-}
+        if (client.count(svc) == 0) {
 
-void grpc_manager::close() {
+            std::cerr << "Connecting gRPC to " << svc << std::endl;
+    
+            auto chan = grpc::CreateChannel(svc,
+                                            grpc::InsecureChannelCredentials());
+            auto cli = std::make_shared<eventstream_client>(chan);
+            client[svc] = cli;
 
-    // Destructors will sort everything.
-    for(auto it = client.begin(); it != client.end(); it++) {
-        it->second->shutdown();
+            cli->start_async();
+    
+        }
+
+        client[svc]->observe(ev);
+
     }
-    client.clear();
-}
 
-grpc_manager::~grpc_manager() {
-}
+    void grpc_manager::close() {
+
+        // Destructors will sort everything.
+        for(auto it = client.begin(); it != client.end(); it++) {
+            it->second->shutdown();
+        }
+        client.clear();
+    }
+
+    grpc_manager::~grpc_manager() {
+    }
 
 }
 
