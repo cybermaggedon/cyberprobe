cf1ca8d5bebca237bedaac38b68b0385fc17f728
Mark Adams <cybermaggedon@gmail.com>
2019-08-15 16:51:15 -0700

    Added gRPC information to docs, gRPC Lua config file.

--- config/grpc.lua
+++ config/grpc.lua
@@ -1,8 +1,16 @@
 
+local os = require("os")
+
+local server = "localhost:50051"
+
+if os.getenv("GRPC_SERVICE") then
+  server = os.getenv("GRPC_SERVICE")
+end
+
 observer = {}
 
 observer.event = function(e)
-  grpc:observe(e, "localhost:50051")
+  grpc:observe(e, server)
 end
 
 return observer
