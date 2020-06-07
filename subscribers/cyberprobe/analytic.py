9ed88da36daf4bc93f4dc7f95f99be7ba5891caf
Mark Adams <cybermaggedon@gmail.com>
2020-06-07 12:19:05 +0100

    Added basic Prometheus metrics describing event processing time and event size.

--- subscribers/cyberprobe/analytic.py
+++ subscribers/cyberprobe/analytic.py
@@ -1,37 +1,29 @@
 
 import pulsar
+from prometheus_client import start_http_server
+from prometheus_client import Counter, Summary, Histogram, Info
 import os
 import sys
 import uuid
 import cyberprobe.cyberprobe_pb2 as pb
 
-def subscribe(binding, handle, output=None):
-
-    broker = os.getenv("PULSAR_BROKER", "pulsar://localhost:6650")
-
-    in_topic = f"persistent://public/default/{binding}"
-    out_topic = f"persistent://public/default/{output}"
-
-    if output != None:
-        outq = Producer(broker, out_topic)
-    else:
-        outq = None
-
-    def output(msg, properties=None):
-        if outq != None:
-            outq.publish(msg, properties)
-        
-    def cb(msg):
-        handle(msg, output)
-
-    subs = str(uuid.uuid4())
-    
-    c = Consumer(subs, broker, in_topic)
-    c.consume(cb)
+request_time=Summary('event_processing_time', 'Time spent processing event')
+event_size=Histogram('event_size', 'Size of event message',
+                     buckets=[25, 50, 100, 250, 500, 1000, 2500, 5000,
+                              10000, 25000, 50000, 100000, 250000, 500000,
+                              1000000, 2500000])
+events=Counter('events_total', 'Events processed total', ['state'])
+info=Info('configuration', 'Configuration settings')
 
 class Analytic:
     def __init__(self, binding, outputs=[]):
-        
+
+        metrication = os.getenv("METRICATION_PORT")
+        if metrication != None:
+            port = int(metrication)
+            start_http_server(port)
+            print(f"Metrics served on port {port}")
+
         broker = os.getenv("PULSAR_BROKER", "pulsar://localhost:6650")
 
         in_topic = f"persistent://public/default/{binding}"
@@ -39,11 +31,28 @@ class Analytic:
         self.outqs = [Producer(broker, v) for v in out_topics]
 
         subs = str(uuid.uuid4())
+
+        info.info({
+            'input_topic': in_topic,
+            'output_topics': " ".join(out_topics),
+            'broker': broker,
+            'subscriber': subs,
+        })
     
         self.cons = Consumer(subs, broker, in_topic)
 
     def run(self):
-        self.cons.consume(self.handle)
+        self.cons.consume(self.dohandle)
+
+    def dohandle(self, msg):
+        try:
+            with request_time.time():
+                event_size.observe(len(msg.data()))
+                self.handle(msg)
+                events.labels('success').inc()
+        except Exception as e:
+            print("Exception:", e)
+            events.labels('failure').inc()
 
     def handle(self, msg):
         pass
@@ -58,12 +67,9 @@ class EventAnalytic(Analytic):
         pass
     
     def handle(self, msg):
-        try:
-            ev = pb.Event()
-            ev.ParseFromString(msg.data())
-            self.event(ev, msg.properties())
-        except Exception as e:
-            print("Exception:", e)
+        ev = pb.Event()
+        ev.ParseFromString(msg.data())
+        self.event(ev, msg.properties())
 
     def output_event(self, ev, properties=None):
         data = ev.SerializeToString()
@@ -89,9 +95,11 @@ class Consumer:
         while True:
             try:
                 msg = self.consumer.receive(200)
-                cb(msg)
             except Exception as e:
-                pass
+                # Pulsar Timeout.  Shame the type is 'Exception' so can't be
+                # distinguished from other events very easily.
+                continue
+            cb(msg)
 
     def close(self):
         self.consumer.unsubscribe()
